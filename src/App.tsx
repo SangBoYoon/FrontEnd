31acda148f4856eefd6d028530de38f09a7dce4c
heeyoon <heeyoon1302@naver.com>
2022-09-26 16:38:20 +0900

    react project 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>도전하는 우리 제법 멋져요?? 우리는 멋쟁이빨대나무TKDJ</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <p>ACCOUNTER</p>
             </header>
         </div>
     );
